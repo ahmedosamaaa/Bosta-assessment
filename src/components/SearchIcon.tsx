fd11175f097b2f2279d78e133a7bf8b9f6cf1686
Ahmed <ahmedosama12313@gmail.com>
2024-12-11 17:54:29 +0200

    change width of searchIcon and change in translation

--- src/components/SearchIcon.tsx
+++ src/components/SearchIcon.tsx
@@ -52,30 +52,35 @@ const SearchIcon = () => {
             {isOpen && (
                 <div
                     ref={divRef}
-                    className={`absolute top-14 left-1/2 transform -translate-x-1/2 z-[1000] w-64 h-24 bg-white p-4 rounded-md shadow-md mt-5 transition-all duration-300 ${
+                    className={`absolute top-14 left-1/2 transform -translate-x-1/2 z-[1000] w-72 sm:w-80 h-36 bg-white p-4 rounded-md shadow-md mt-5 transition-all duration-300 ${
                         isClosing
                             ? "opacity-0 scale-90"
                             : "opacity-100 scale-100"
                     }`}
                 >
-                    <div className="flex flex-row justify-center">
-                        <input
-                            type="text"
-                            id="search"
-                            className="h-12 p-4 text-sm text-gray-900 border border-transparent border-gray-300 rounded-lg shadow-md w-44 bg-gray-50 focus:ring-0 focus:border-transparent focus-visible:outline-blue-400"
-                            placeholder={t("search")}
-                            defaultValue={trackingNumber ?? ""}
-                            onChange={onSearchChange}
-                        />
+                    <div className="flex flex-col items-center justify-center gap-4">
+                        <p className="self-start text-sm text-gray-600 ps-2">
+                            Track your shipment
+                        </p>
+                        <div className="flex flex-row justify-center">
+                            <input
+                                type="text"
+                                id="search"
+                                className="h-12 p-4 text-sm text-gray-900 border border-transparent border-gray-300 rounded-lg shadow-md w-52 sm:w-60 bg-gray-50 focus:ring-0 focus:border-transparent focus-visible:outline-blue-400"
+                                placeholder={t("search")}
+                                defaultValue={trackingNumber ?? ""}
+                                onChange={onSearchChange}
+                            />
 
-                        <button
-                            onClick={handleSubmit}
-                            className={`flex items-center justify-center  bg-red-600 ${
-                                isRTL ? "-left-10" : "-right-8"
-                            } w-10 rounded-e-md`}
-                        >
-                            <IoSearchSharp size={25} color="#fff" />
-                        </button>
+                            <button
+                                onClick={handleSubmit}
+                                className={`flex items-center justify-center  bg-red-600 ${
+                                    isRTL ? "-left-10" : "-right-8"
+                                } w-10 rounded-e-md`}
+                            >
+                                <IoSearchSharp size={25} color="#fff" />
+                            </button>
+                        </div>
                     </div>
                 </div>
             )}
