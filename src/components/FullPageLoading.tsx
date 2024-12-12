e1357d6682a3cc62bd751c35d74d9c622e1e9ded
Ahmed <ahmedosama12313@gmail.com>
2024-12-12 14:54:32 +0200

    Change The Color OF Full Page Loader Component

--- src/components/FullPageLoading.tsx
+++ src/components/FullPageLoading.tsx
@@ -1,6 +1,6 @@
 function FullPageLoading() {
     return (
-        <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+        <div className="absolute inset-0 flex items-center justify-center bg-background-color backdrop-blur-sm">
             <div className="relative inline-flex">
                 <div className="w-8 h-8 bg-red-500 rounded-full"></div>
                 <div className="absolute top-0 left-0 w-8 h-8 bg-red-500 rounded-full animate-ping"></div>
